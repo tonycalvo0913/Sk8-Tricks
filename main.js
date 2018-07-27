71c66982856c00cf42efd44ed1082d07beb73397
Tony Calvo <anthonycalvo0913@gmail.com>
2018-07-26 18:36:28 -0700

    trying to catch up

--- main.js
+++ main.js
@@ -3,14 +3,14 @@
 //This is the url from the Airtable Authentication section
 var airtable_list_url = 'https://api.airtable.com/v0/appG4GemUBkB0ygAy/Table%201?api_key=keyB7EQxA9Lt2lvXE';
 
-var cardTemplate = function(name, address, picture) {
+var cardTemplate = function(trickname, category, image) {
     return `
       <div class="card col-sm-4">
-        <img src="${picture}" class="card-img-top"alt="Card image cap">
+        <img src="${image}" class="card-img-top"alt="Card image cap">
         <div class="card-body">
-          <h5 class="card-title">${name}</h5>
-          <p class="card-text">${address}</p>
-          <a href="#" class="btn btn-primary">See Details</a>
+          <h5 class="card-title">${trickname}</h5>
+          <p class="card-text">${category}</p>
+          <a href="#" class="btn btn-primary">Trick</a>
         </div>
       </div>`;
     }
@@ -21,7 +21,7 @@ $.getJSON( airtable_list_url, function( data ) {
   var items = [];
   $.each( data.records, function( key, val ) {
     console.log(val.fields)
-    items.push(`<h2>${val.fields['Trick Name']}</h2>`);
+    items.push(`<h2>${val.fields['TrickName']}</h2>`);
   });
   $(".list-view").append(items.join(''));
 });
