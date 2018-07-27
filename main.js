3e85e71a02a6e8dbca9c697c00b240a66fcc9120
Tony Calvo <anthonycalvo0913@gmail.com>
2018-07-26 18:31:55 -0700

    try to catch up

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
+//API Key from Airtable 
+//var api_key = 'keyB7EQxA9Lt2lvXE'
 //This is the url from the Airtable Authentication section
 var airtable_list_url = 'https://api.airtable.com/v0/appG4GemUBkB0ygAy/Table%201?api_key=keyB7EQxA9Lt2lvXE';
 
+var cardTemplate = function(name, address, picture) {
+    return `
+      <div class="card col-sm-4">
+        <img src="${picture}" class="card-img-top"alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">${name}</h5>
+          <p class="card-text">${address}</p>
+          <a href="#" class="btn btn-primary">See Details</a>
+        </div>
+      </div>`;
+    }
+    
 // This is where we get the JSON data from Airtable!
 
 $.getJSON( airtable_list_url, function( data ) {
