a265762039818f1b20e92268a34daf8de97f9fd9
Tony Calvo <anthonycalvo0913@gmail.com>
2018-08-02 17:32:21 -0700

    trial one of wht i did

--- main.js
+++ main.js
@@ -3,26 +3,28 @@
 //This is the url from the Airtable Authentication section
 var airtable_list_url = 'https://api.airtable.com/v0/appG4GemUBkB0ygAy/Table%201?api_key=keyB7EQxA9Lt2lvXE';
 
-var listView = function(id, trickname, image, category) {
+var listView = function(id, trickname, difficulty, category) {
     return `<div class="col-sm-3">
-      <div class="card mb-4 box-shadow">
-        <a href="?id=${id}"><img class="card-img-top" src="${image}"></a>
         <div class="card-body">
           <h2><a href="?id=${id}">${trickname}</a></h2>
           <div class="d-flex justify-content-between align-items-center">
             <small class="text-muted">${category}</small>
           </div>
         </div>
-      </div>
     </div>`;
   }
 // This is where we get the JSON data from Airtable!
 
 $.getJSON( airtable_list_url, function( data ) {
-  var items = [];
-  $.each( data.records, function( key, val ) {
-    console.log(val.fields)
-    items.push(`<h2>${val.fields['TrickName']}</h2>`);
+  var html = [];
+  html.push(`<div class="row">`);
+  // 2. Iterates over every record and uses the list template
+  $.each( data.records, function( index, val ) {
+    // console.log(val.fields)
+    var id = val.id;
+    var fields = val.fields;
+    var trickname = fields["TrickName"];
+    var imageUrl = fields["Image"] ? fields["Image"][0].url : '';
   });
   $(".list-view").append(items.join(''));
 });
