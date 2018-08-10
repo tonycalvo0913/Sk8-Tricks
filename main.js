ccc430c0054ab1b5a74368222d9b99320e6faa72
Tony Calvo <anthonycalvo0913@gmail.com>
2018-08-09 17:24:10 -0700

    detailed view

--- main.js
+++ main.js
@@ -18,12 +18,12 @@ var listView = function(id, trickname, difficulty, category) {
  var getDataForList = function() {  
   // This is where we get the JSON data from Airtable!
   $.getJSON( airtable_list_url, function( data ) {
-    console.log(data.records);
+    //console.log(data.records);
     var html = [];
     html.push(`<div class="row">`);
     // 2. Iterates over every record and uses the list template
     $.each( data.records, function( index, val ) {
-     console.log(val.fields)
+     //console.log(val.fields)
         var id = val.id;
         var fields = val.fields;
         var trickname = fields["TrickName"];
@@ -34,15 +34,15 @@ var listView = function(id, trickname, difficulty, category) {
     });
     html.push(`</div>`);
 	// 3. Adds HTML for every item to our page
-  $(".list-view").append(html.join(''));
+  $(".list-view").append(html.join(""));
 });
 }
 
 // Template that generates HTML for one item in our detail view, given the parameters passed in
-var detailView = function(id, trickname, imageUrl, difficulty, category, tips, instructions) {
+var detailView = function(id, trickname, pictureUrl, difficulty, category, tips, instructions) {
   return `<div class="col-sm-12">
     <div class="card mb-4 box-shadow">
-      <img class="card-img-top" src="${imageUrl}">
+      <img class="card-img-top" src="${pictureUrl}">
       <div class="card-body">
         <h2>${trickname}</h2>
         <p class="card-text">${category}</p>
@@ -55,4 +55,37 @@ var detailView = function(id, trickname, imageUrl, difficulty, category, tips, i
       </div>
     </div>
   </div>`;
+}
+
+// Get and display the data for one item based on on the ID
+var getDataForId = function(id) {
+  $.getJSON( `https://api.airtable.com/v0/appG4GemUBkB0ygAy/Table%201?${id}?api_key=${keyB7EQxA9Lt2lvXE}`, function( record ) {
+    // console.log(data);
+    var html = [];
+    html.push(`<div class="row">`);
+      // console.log(val)
+      var id = record.id;
+      var fields = record.fields;
+      var trickname = fields["TrickName"];
+      var pictureUrl = fields["Image"] ? fields["Image"][0].url : '';
+      var difficulty = fields["Difficulty"];
+      var category = fields["Category"];
+      var  instructions = fields["Instructions"];
+      var tips= fields["Category"];
+      var itemHTML = detailView(id, trickname, pictureUrl, instructions, difficulty, category, tips);
+      html.push(itemHTML);
+    html.push(`</div>`);
+    $(".detail-view").append(html.join(""));
+  });
+}
+
+// Do we have an ID in the URL?
+var id = getParameterByName("id");
+
+// If we have an ID, we should only get the data for one item
+// Otherwise, we should display the data for all items
+if (id) {
+  getDataForId(id);
+} else {
+  getDataForList();
 }
